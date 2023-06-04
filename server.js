eaa872362b30a8872b7af68d0a62b205d0bdb830
HanineGuesmi <guesmihanin@takiacademyteam.com>
2023-06-04 22:50:42 +0100

    build update && get requests

--- server.js
+++ server.js
@@ -11,17 +11,22 @@ app.post('/add', (req, res)=>{
 
 app.get('/getAll', (req, res)=>{
     console.log('get work');
-    res.send('Add request received'); // Send a response to the client
+    res.send('Add request received')
 });
 
 app.put('/update', (req, res)=>{
-    console.log('get work');
-    res.send('Add request received'); // Send a response to the client
-});
+    console.log('update work');
+    res.send('Add request received'); 
+})
+
+app.delete('/delete', (req, res)=>{
+    console.log('delete work');
+    res.send('delete request received'); 
+})
 
 app.listen( 3000, ()=>{ 
     console.log ('sever work');
-}); 
+})
  //Listen is an  express function  order   to keep the server always running unless we stop it it take 2 paramùs the port and thed function 
 
  
