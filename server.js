789937e5022bb031b25784cbb0a71a94fd87dc12
HanineGuesmi <guesmihanin@takiacademyteam.com>
2023-06-04 22:41:13 +0100

    add request

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("hello");
+const express = require('express')
+const app = express() //import express library 
+
+app.use(express.json()); //to parse JSON data
+
+
+app.post('/add', (req, res)=>{
+    console.log('add work');
+    res.send('Add request received'); // Send a response to the client
+});
+
+app.listen( 3000, ()=>{ 
+    console.log ('sever work');
+}); 
+ //Listen is an  express function  order   to keep the server always running unless we stop it it take 2 paramùs the port and thed function 
+
+ 
